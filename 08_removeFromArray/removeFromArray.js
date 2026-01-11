f1f25379e26eab9243dddcb64d19b322ac55d300
Tristan <tristanacosta03@gmail.com>
2026-01-11 17:58:32 +0800

    Finish removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    let newArray = [];
+
+    array.map(element => {
+        if (element === remove) {
+            return;
+        }
+        else {
+            newArray.push(element);
+        }
+    });
+
+    return newArray;
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
