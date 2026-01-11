a76e2cf074c0d77004399a29c772215d0dc051fd
Tristan <tristanacosta03@gmail.com>
2026-01-11 11:39:21 +0800

    Finish reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let splitString = string.split("");
+    let reversed = splitString.reverse();
+    let reversedString = reversed.join("");
+    
+    console.log(reversedString);
+    return reversedString;
 };
+
+console.log(reverseString("hello there"));
 
 // Do not edit below this line
 module.exports = reverseString;
