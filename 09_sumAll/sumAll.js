57574b095e9fe753856f6d109a020047c3458c7c
Tristan <tristanacosta03@gmail.com>
2026-01-13 09:50:30 +0800

    Finish sumAll exercise

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let totalSum = 0;
+    let rangeStart = 0;
+    let rangeEnd = 0;
 
+    if (numOne > numTwo) {
+        rangeStart = numTwo;
+        rangeEnd = numOne;
+    }
+    else {
+        rangeStart = numOne;
+        rangeEnd = numTwo;
+    }
+    
+    if ((numOne < 0 || numTwo < 0) || (!Number.isInteger(numOne) || (!Number.isInteger(numTwo)))) {
+        return "ERROR";
+    }
+
+    for (; rangeStart <= rangeEnd; rangeStart++) {
+        totalSum += rangeStart;
+    }
+
+    return totalSum;
 };
 
 // Do not edit below this line
