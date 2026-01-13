a4dad0faab78a8c986346ee4068b7803e396d10a
Tristan <tristanacosta03@gmail.com>
2026-01-13 09:48:00 +0800

    Finish tempConversion exercise

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  num = (num - 32) * (5 / 9);
+  let roundedNum = Math.round(num * 10) / 10;
+  return roundedNum;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  num = (num * (9 / 5)) + 32;
+  let roundedNum = Math.round(num * 10) / 10;
+  return roundedNum;
 };
 
 // Do not edit below this line
