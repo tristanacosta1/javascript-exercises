b57145aa535bc10b89efccc18ad9b175c4206c99
Tristan <tristanacosta03@gmail.com>
2026-08-11 19:39:05 +0800

    Implement factorial function

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,12 @@
-const factorial = function() {
-
+const factorial = function(i) {
+    if (!Number.isInteger(i) || i < 0) {
+        return undefined;
+    }
+    if (i === 0) {
+        return 1;
+    } else {
+        return i * factorial(i - 1);
+    }
 };
 
 // Do not edit below this line
