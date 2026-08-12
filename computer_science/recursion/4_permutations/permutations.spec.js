937e8a30eb730117797d7756d76af50f32fb0615
Tristan <tristanacosta03@gmail.com>
2026-08-12 20:57:43 +0800

    Remove skips from tests to test permutations

--- computer_science/recursion/4_permutations/permutations.spec.js
+++ computer_science/recursion/4_permutations/permutations.spec.js
@@ -1,11 +1,11 @@
 const permutations = require("./permutations");
 
 describe("permutations", () => {
-  test.skip("1 possible permutation for a set containing 0 numbers", () => {
+  test("1 possible permutation for a set containing 0 numbers", () => {
     expect(permutations([])).toEqual([[]]);
   });
 
-  test.skip("1 possible permutation for a set containing 1 number", () => {
+  test("1 possible permutation for a set containing 1 number", () => {
     expect(permutations([1])).toEqual([[1]]);
   });
 
@@ -18,7 +18,7 @@ describe("permutations", () => {
     );
   });
 
-  test.skip("6 possible permutations for a set containing 3 numbers", () => {
+  test("6 possible permutations for a set containing 3 numbers", () => {
     expect(permutations([1, 2, 3]).sort()).toEqual(
       [
         [1, 2, 3],
@@ -31,7 +31,7 @@ describe("permutations", () => {
     );
   });
 
-  test.skip("24 possible permutations for a set containing 4 numbers", () => {
+  test("24 possible permutations for a set containing 4 numbers", () => {
     expect(permutations([1, 2, 3, 4]).sort()).toEqual(
       [
         [1, 2, 3, 4],
