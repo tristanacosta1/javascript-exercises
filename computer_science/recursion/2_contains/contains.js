86fd91fe4bf14c82e8f4cfa71c4c5805dace65f5
Tristan <tristanacosta03@gmail.com>
2026-08-12 10:08:50 +0800

    Implement contains function

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,6 +1,18 @@
-const contains = function() {
-  
-};
+const contains = function(obj, val) {
+	if (obj === val) {
+		return true;
+	}
+	if (Object.prototype.toString.call(obj) === '[object Object]') {
+		for (const v of Object.values(obj)) {
+			if (contains (v, val)) {
+				return true
+			}
+		}
+		return false;
+	} else {
+		return false;
+	}
+}
   
 // Do not edit below this line
 module.exports = contains;
