1727d364a282abfc876892b1df959c98a19aeead
Tristan <tristanacosta03@gmail.com>
2026-08-13 21:22:02 +0800

    Refactor totalIntegers to fix input bug

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,23 +1,26 @@
 const totalIntegers = function(d) {
-  if (Number.isInteger(d)) {
-    return 1;
-  }
   if (Array.isArray(d)) {
-    let total = 0;
+  let total = 0;
     for (const e of d) {
-        total += totalIntegers(e);
+        if (Number.isInteger(e)) {
+          total++;
+        } else if (Array.isArray(e) || Object.prototype.toString.call(e) === '[object Object]'){
+          total += totalIntegers(e);
+        }
     }
     return total;
   } else if (Object.prototype.toString.call(d) === '[object Object]') {
-    let total = 0;
+  let total = 0;
     for (const v of Object.values(d)) {
+      if (Number.isInteger(v)) {
+        total++;
+      } else if (Array.isArray(v) || Object.prototype.toString.call(v) === '[object Object]'){
         total += totalIntegers(v);
+      }
     }
     return total;
-  } else {
-    return 0;
   }
-};
+}
   
 // Do not edit below this line
 module.exports = totalIntegers;
