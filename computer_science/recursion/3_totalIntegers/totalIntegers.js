366cd7fca5d8a410a75d2f4879aaa73df379c6e2
Tristan <tristanacosta03@gmail.com>
2026-08-13 11:16:14 +0800

    Implement totalIntegers funciton

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,22 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(d) {
+  if (Number.isInteger(d)) {
+    return 1;
+  }
+  if (Array.isArray(d)) {
+    let total = 0;
+    for (const e of d) {
+        total += totalIntegers(e);
+    }
+    return total;
+  } else if (Object.prototype.toString.call(d) === '[object Object]') {
+    let total = 0;
+    for (const v of Object.values(d)) {
+        total += totalIntegers(v);
+    }
+    return total;
+  } else {
+    return 0;
+  }
 };
   
 // Do not edit below this line
